267ef9d6917e92f5a305173e267e3f08568254e1
bryon burke <bryonburke@bryons-MacBook-Pro.local>
2020-03-06 11:41:15 -0800

    bad comp separation

--- App.js
+++ App.js
@@ -1,38 +1,31 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState('');
+  
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
+
 
   const addGoalHandler = () => {
     console.log(enteredGoal);
-    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
-  }
+    setCourseGoals(currentGoals => [
+      ...currentGoals, 
+      { key: Math.random().toString(), value: enteredGoal}
+    ]);
+  };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput style={styles.input} 
-          placeholder="Course Goals"  
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
+      <GoalInput />
+      <FlatList
+        data={courseGoals}
+        renderItem={itemData => <GoalItem title={itemData.item.value} />}
         />
-        <Button title="ADD" onPress={addGoalHandler}/>
-      </View>
-      <ScrollView> 
-        {courseGoals.map((goal) => (
-          <View key={goal} style={styles.listItems}>
-            <Text >{goal}</Text>
-          </View>
-        ))}
-      </ScrollView>
-      
-    </View>
+    </View >
   );
 }
 
@@ -40,25 +33,8 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50
   },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center' 
-  },
-  input: {
-    width: '80%', 
-    borderColor: 'black', 
-    borderWidth: 1, 
-    padding: 10
-  },
 
-  listItems: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
-  }
+
 
 
 });
